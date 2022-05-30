00472a670e2d5d72ba6021d27ec29f1f4ef04b40
Hans <bangguducks@gmail.com>
2022-05-30 22:17:30 +0900

    TODO

--- gallery/src/main/java/com/coding/gallery/Service/MemberServiceImpl.java
+++ gallery/src/main/java/com/coding/gallery/Service/MemberServiceImpl.java
@@ -4,7 +4,9 @@ import com.coding.gallery.Entity.Member;
 import com.coding.gallery.Repository.MemberRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MemberServiceImpl {
     @Autowired
     private MemberRepository memberRepository;
