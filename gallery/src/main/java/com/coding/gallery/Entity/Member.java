b5bea1d6158b344d5ae62060ca21ff2cf6b9d19d
Hans <bangguducks@gmail.com>
2022-05-30 22:16:55 +0900

    TODO

--- gallery/src/main/java/com/coding/gallery/Entity/Member.java
+++ gallery/src/main/java/com/coding/gallery/Entity/Member.java
@@ -1,7 +1,6 @@
 package com.coding.gallery.Entity;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 import lombok.Getter;
---
+++ gallery/src/main/java/com/coding/gallery/Service/MemberServiceImpl.java
@@ -0,0 +1,19 @@
+package com.coding.gallery.Service;
+
+import com.coding.gallery.Entity.Member;
+import com.coding.gallery.Repository.MemberRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class MemberServiceImpl {
+    @Autowired
+    private MemberRepository memberRepository;
+
+    // TODO : Spring Security를 이용하여 개발 예정
+
+    public Member getMember(Member member)
+    {
+        return null;
+    }
+    
+}
